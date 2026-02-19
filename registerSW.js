25d0e0a3d9d6a9e1530f4cb1ac2336a190d14d92
Leo <leolee589297@gmail.com>
2026-02-20 01:22:19 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Bussiness-Agent/sw.js', { scope: '/Bussiness-Agent/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
